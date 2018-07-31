db064d4cb172cbd2f2afa122ed22ee0a63833bb0
vietson6103@gmail.com <Hn0912388240>
2018-07-31 16:16:17 +0700

    add Customer Servlet

---
+++ src/main/java/com/codegym/controller/CustomerServlet.java
@@ -0,0 +1,14 @@
+package com.codegym.controller;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "com.codegym.controller.CustomerServlet")
+public class CustomerServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
